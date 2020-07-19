46bc6822f0e7c7c94b6e3ed72f204fe81a75f241
Andres Lemus <aclmadrid04@gmail.com>
2020-07-18 23:04:57 -0500

    refac(front): #4.4 improve search page

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -2,30 +2,44 @@ import React from "react";
 import { SecondaryCard } from "../../components/SecondaryCard";
 import { dummyMovies } from "../Premier/dummy";
 import { DropdownButton } from "../../components/DropdownButton";
+import { genres } from "./dummy";
 
-export const Search = (): React.ReactElement => (
-  <section className="px-5 max-w-6xl pt-24 md:mx-4 lg:mx-auto">
-    <div className="flex justify-around items-center">
-      <h2 className="md:pl-16 uppercase text-lg text-gray-800 font-light tracking-wider">
-        Films
-      </h2>
-      <div className="flex justify-center">
-        {[1, 2, 3, 4, 5].map((key) => (
-          <DropdownButton key={key} />
+const filters = ["country", "genre", "language", "year"];
+
+const genresOptions = genres.map((genre) => ({
+  value: genre.id.toString(),
+  label: genre.name,
+}));
+
+export const Search = (): React.ReactElement => {
+  return (
+    <section className="px-5 max-w-6xl md:pt-24 md:mx-4 lg:mx-auto">
+      <div className="flex flex-col md:flex-row justify-around items-center">
+        <h2 className="lg:pl-16 uppercase text-lg text-gray-800 font-light tracking-wider">
+          Films
+        </h2>
+        <div className="pt-5 md:p-0 flex flex-wrap sm:flex-no-wrap justify-center max-w-xl">
+          {filters.map((filter) => (
+            <DropdownButton
+              key={filter}
+              label={filter}
+              options={genresOptions}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="pt-12 pb-40 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 col-gap-4 row-gap-6 place-items-center grid-flow-row-dense">
+        {dummyMovies.map((movie) => (
+          <SecondaryCard
+            key={movie.id}
+            imageUrl={movie.poster_path}
+            title={movie.title}
+            language={movie.original_language}
+            releaseDate={movie.release_date}
+            voteAvg={movie.vote_average}
+          />
         ))}
       </div>
-    </div>
-    <div className="pt-12 pb-40 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 col-gap-4 row-gap-6 place-items-center grid-flow-row-dense">
-      {dummyMovies.map((movie) => (
-        <SecondaryCard
-          key={movie.id}
-          imageUrl={movie.poster_path}
-          title={movie.title}
-          language={movie.original_language}
-          releaseDate={movie.release_date}
-          voteAvg={movie.vote_average}
-        />
-      ))}
-    </div>
-  </section>
-);
+    </section>
+  );
+};
