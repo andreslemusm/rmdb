936f4d55a983bb076f4c01cc708ec72b44abd3bc
Andres Lemus <aclmadrid04@gmail.com>
2020-07-18 23:03:11 -0500

    feat(front): #4.3 add DropdownButton component

--- src/components/DropdownButton/index.tsx
+++ src/components/DropdownButton/index.tsx
@@ -1,23 +1,39 @@
 import React from "react";
+import { ReactComponent as CaretDown } from "../../assets/caret-down.svg";
 
-export const DropdownButton = (): React.ReactElement => (
-  <div className="relative mr-1">
-    <select
-      className="appearance-none w-full bg-transparent border border-gray-800 text-gray-800 py-1 pl-3 pr-6 focus:outline-none"
-      id="grid-state"
-    >
-      <option>language</option>
-      <option>Missouri</option>
-      <option>Texas</option>
-    </select>
-    <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-800">
-      <svg
-        className="fill-current h-4 w-4"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
+type DropdownButtonProps = {
+  label: string;
+  options?: { value: string; label: string }[];
+};
+
+export const DropdownButton = ({
+  label,
+  options,
+}: DropdownButtonProps): React.ReactElement => {
+  const [value, setValue] = React.useState("");
+
+  const handleBlur = (event: React.FocusEvent<HTMLSelectElement>): void => {
+    setValue(event.target.value);
+  };
+
+  return (
+    <div className="relative mt-2 sm:m-0 sm:mr-1 w-full">
+      <select
+        defaultValue={value}
+        className="appearance-none w-full bg-transparent border border-gray-800 text-gray-800 py-1 pl-3 pr-6 lowercase focus:outline-none "
+        onBlur={handleBlur}
       >
-        <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-      </svg>
+        <option value={value}>{label}</option>
+        {options !== undefined &&
+          options.map((option) => (
+            <option value={option.value} key={option.value}>
+              {option.label}
+            </option>
+          ))}
+      </select>
+      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-800">
+        <CaretDown />
+      </div>
     </div>
-  </div>
-);
+  );
+};
