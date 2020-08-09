7bc223bb85c8c4da6a352aa55d62bd0266753f0e
Andres Lemus <aclmadrid04@gmail.com>
2020-08-09 18:46:28 -0500

    feat(front): #3.4 create use_thunk_reducer

---
+++ src/utils/hooks/useThunkReducer.ts
@@ -0,0 +1,19 @@
+import React from "react";
+
+type Thunk<T> = (dispatch: React.Dispatch<T>) => void;
+
+export function useThunkReducer<S, A>(
+  reducer: React.Reducer<S, A>,
+  initialState: S
+): readonly [S, (action: A | Thunk<A>) => void] {
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+
+  const enhancedDispatch = React.useCallback((action: A | Thunk<A>): void => {
+    if (typeof action === "function") {
+      return (action as Thunk<A>)(dispatch);
+    }
+    return dispatch(action);
+  }, []);
+
+  return [state, enhancedDispatch] as const;
+}
