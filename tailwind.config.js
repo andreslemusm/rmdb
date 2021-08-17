d182d8bdf3e829858ef7fe1d85df6ccae8a97654
Andres Lemus <aclmadrid04@gmail.com>
2021-08-16 19:27:49 -0500

    feat: update deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
+  mode: "jit",
   purge: ["src/**/*.ts", "src/**/*.tsx", "public/**/*.html"],
   theme: {
     fontFamily: {
