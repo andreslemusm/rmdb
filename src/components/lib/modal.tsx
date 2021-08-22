b616cdbf26128532112706c73d48c53ca61b6835
Andres Lemus <aclmadrid04@gmail.com>
2021-08-22 15:26:43 -0500

    fix(trailer-modal): overflow on mobile screens
    re #13

--- src/components/lib/modal.tsx
+++ src/components/lib/modal.tsx
@@ -19,7 +19,7 @@ export const Modal = ({
       onClose={onClose}
       initialFocus={initialFocus}
     >
-      <div className="min-h-screen pt-4 px-4 pb-20 text-center sm:p-0">
+      <div className="min-h-screen pt-4 px-4 pb-20 text-center">
         <Transition.Child
           as={Fragment}
           enter="ease-out duration-300"
@@ -39,11 +39,11 @@ export const Modal = ({
         <Transition.Child
           as={Fragment}
           enter="ease-out duration-300"
-          enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-          enterTo="opacity-100 translate-y-0 sm:scale-100"
+          enterFrom="opacity-0 translate-y-4"
+          enterTo="opacity-100 translate-y-0"
           leave="ease-in duration-200"
-          leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-          leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          leaveFrom="opacity-100 translate-y-0"
+          leaveTo="opacity-0 translate-y-4"
         >
           <div className="inline-block align-middle bg-secondary rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-2xl transform transition-all my-8 max-w-6xl w-full sm:p-6">
             {children}
