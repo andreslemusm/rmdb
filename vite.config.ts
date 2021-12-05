d4ffafe793eec7ddf1a39e91d7b207e7f77b831e
Andres Lemus <alemus@lean-tech.io>
2021-12-05 13:37:04 -0500

    build: add tsconfig paths plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,37 +3,13 @@
   Default export is required by vite
 */
 import { defineConfig } from "vite";
-import path from "path";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+
 
 /**
  * @type {import('vite').UserConfig}
  */
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [
-      { find: "@root", replacement: path.resolve(__dirname, "./src") },
-      {
-        find: "@assets",
-        replacement: path.resolve(__dirname, "./src/assets"),
-      },
-      {
-        find: "@components",
-        replacement: path.resolve(__dirname, "./src/components"),
-      },
-      {
-        find: "@services",
-        replacement: path.resolve(__dirname, "./src/services"),
-      },
-      {
-        find: "@pages",
-        replacement: path.resolve(__dirname, "./src/pages"),
-      },
-      {
-        find: "@utils",
-        replacement: path.resolve(__dirname, "./src/utils"),
-      },
-    ],
-  },
+  plugins: [react(), tsconfigPaths()],
 });
