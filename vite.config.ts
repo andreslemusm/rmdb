6c1f3c8b8d4dcf595be1431fb3a294ccae3f485e
Andres Lemus <aclmadrid04@gmail.com>
2022-03-01 14:13:14 -0500

    feat: add paths plugin

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-
-/**
- * @type {import('vite').UserConfig}
- */
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
 });
